e83461c0aa94e6c49196fa392b39c18c061b6f0b
Devin Nelly <ismail_laabab@um5.ac.ma>
2025-10-24 13:51:58 +0800

    Create module initialization code for version 6.2.3

---
+++ v6_2_3/modules/__init__.py
@@ -0,0 +1,7 @@
+# Module initialization for version 6.2.3
+
+# This is the initialization code for the q6 package version 6.2.3.
+
+__version__ = '6.2.3'
+
+# Add any additional initialization code here if necessary.
